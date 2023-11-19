ec2a6e7bb01110818b3de68b42b46aa26ef1928d
Pepo2405 <ignacioniglesias@gmail.com>
2023-11-18 23:23:30 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import {nodePolyfills} from 'vite-plugin-node-polyfills'
-
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: {
-      process: "process/browser",
-      stream: "stream-browserify",
-      zlib: "browserify-zlib",
-      util: 'util'
-    }
+    mainFields: ['browser', 'module', 'main']
   },
-  plugins: [
-    svelte(),
-    nodePolyfills(),
-
-  ],
-})
+  plugins: [svelte(),nodePolyfills()],
+});
