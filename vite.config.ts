61251e82f78cfa0cfe28398b8e9eebe422878793
Pepo2405 <ignacioniglesias@gmail.com>
2023-11-18 23:17:39 -0300

    poly

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {nodePolyfills} from 'vite-plugin-node-polyfills'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +13,9 @@ export default defineConfig({
       util: 'util'
     }
   },
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    nodePolyfills(),
+
+  ],
 })
