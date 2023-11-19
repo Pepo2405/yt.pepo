bb79db00f3b48c70f8dd93309e717e9e273c29ec
Pepo2405 <ignacioniglesias@gmail.com>
2023-11-18 23:13:36 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      process: "process/browser",
+      stream: "stream-browserify",
+      zlib: "browserify-zlib",
+      util: 'util'
+    }
+  },
   plugins: [svelte()],
 })
